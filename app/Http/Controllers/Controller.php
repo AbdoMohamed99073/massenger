8ba6bad8da020b27e8986243971b052a2d4582cd
AbdoMohamed99073 <abdel99073616@gmail.com>
2024-07-11 16:02:12 +0200

    add relation between models

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,8 +1,15 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Auth;
 
 abstract class Controller
 {
-    //
+    public function index()
+    {
+        $user = Auth::user();
+    }
+
+    
+
 }
